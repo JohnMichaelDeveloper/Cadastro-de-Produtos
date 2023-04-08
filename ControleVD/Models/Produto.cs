b2dcaf8f5b63c47f0e4a0eebf503aaab556c0e8d
John Michael <johnfrontend@gmail.com>
2023-04-08 12:20:33 -0300

    Segundo Commit - Model Produto

--- ControleVD/Models/Produto.cs
+++ ControleVD/Models/Produto.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ControleVD.Models
 {
+    [Table("Produto")]
     public class Produto
     {
+        [Display(Name ="Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
     }
 }
