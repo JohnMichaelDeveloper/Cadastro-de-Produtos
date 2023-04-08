ff03c5cb43c8bb9c053ba2023f636b15c23a27a4
John Michael <johnfrontend@gmail.com>
2023-04-08 12:31:31 -0300

    Terceiro Commit - Configurando DbContext e CreateldentitySchema

--- ControleVD/Data/ApplicationDbContext.cs
+++ ControleVD/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ControleVD.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace ControleVD.Data
             : base(options)
         {
         }
+
+        public DbSet<Produto> Produto { get; set; }
     }
 }
--- ControleVD/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ ControleVD/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -8,6 +8,22 @@ namespace ControleVD.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false).Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(maxLength: 256, nullable: true),
+                   
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+
+
+
             migrationBuilder.CreateTable(
                 name: "AspNetRoles",
                 columns: table => new
